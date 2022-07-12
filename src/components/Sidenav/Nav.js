47a237153603db64653890459fff4a81d647e630
kartavya <kartavyas@gmail.com>
2022-07-12 16:20:25 +1000

    component folder added with images

---
+++ src/components/Sidenav/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./nav.css";
+
+const Nav = () => {
+  return (
+    <div className="side-nav">
+      <img src="../images/avatar.png" alt="tech"></img>
+      <h2>Kartavya Saini</h2>
+      <h2> About </h2>
+      <h2> Projects </h2>
+      <h2> Skills</h2>
+      <h2> Contact </h2>
+      <h2> Resume </h2>
+    </div>
+  );
+};
+
+export default Nav;
