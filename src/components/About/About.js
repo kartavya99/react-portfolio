4a19ce75bb3c7aa67b151bcb740944e96f95a4d8
kartavya <kartavyas@gmail.com>
2022-07-15 18:35:41 +1000

    added more style and re-structure skills

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,30 +5,33 @@ import "./About.css";
 const About = () => {
   return (
     <div className="about">
-      <div className="para">
-        <p className="text">
-          I am a full stack developer student with 10+ years experience in sales
-          and marketing.
+      <div className="para-about">
+        <p className="text-about">
+          Full stack web developer leveraging a background in sales and
+          marketing to provide the best user experience for web application
+          development.
         </p>
       </div>
-      <div className="para">
-        <p className="text">
-          At present I am studying Full stack web development at Monash
-          University and during the boot Camp I have gained good understanding
-          of computer science applied to JavaScript, browser based technologies
-          HTML, CSS, JavaScript, JQuery and Git the version control system.
+      <div className="para-about">
+        <p className="text-about">
+          Currently completing a Certificate in Full Stack Web Development at
+          Monash University with skills in HTML, CSS, JavaScript, MongoDB,
+          React, ExpressJs, NodeJS. Adaptive problem-solver who loves to develop
+          apps with focus on mobile-first design and development.c I have gained
+          good understanding of computer science applied to JavaScript, browser
+          based technologies HTML, CSS, JavaScript, JQuery and Git the version
+          control system.
         </p>
       </div>
-      <div className="para">
-        <p className="text">
-          At present I am studying Full stack web development at Monash
-          University and during the boot Camp I have gained good understanding
-          of computer science applied to JavaScript, browser based technologies
-          HTML, CSS, JavaScript, JQuery and Git the version control system.
+      <div className="para-about">
+        <p className="text-about">
+          Strengths in adaptability, detail-orientation, creativity,
+          communication, problem-solving, teamwork, time-management, leadership,
+          perseverance, and building projects from concept to completion.
         </p>
       </div>
-      <div className="para">
-        <p className="text">icons</p>
+      <div className="para-about">
+        <p className="text-about">icons</p>
       </div>
     </div>
   );
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,29 +4,31 @@ import "./Skill.css";
 const Skills = () => {
   return (
     <div className="skill">
-      <div className="para">
+      <div className="para-skills ">
         <h3 className="heading">Languages</h3>
-        <h6 className="text">HTML, CSS, JavaScript, SQL</h6>
+        <h6 className="text-skills">HTML / CSS / JavaScript / SQL</h6>
       </div>
-      <div className="para">
+      <div className="para-skills ">
         <h3 className="heading">Frontend</h3>
-        <h6 className="text">React, Bootstrap, JQUery, Redux</h6>
+        <h6 className="text-skills">React / Bootstrap / JQuery / Redux</h6>
       </div>
-      <div className="par">
+      <div className="para-skills ">
         <h3 className="heading">Backend</h3>
-        <h6 className="text">
-          Node.js, ExpressJS, REST APIs, Apollo, MongoDB, Mongoose, Sequelize{" "}
+        <h6 className="text-skills">
+          Node.js / ExpressJS / REST APIs / Apollo / MongoDB / Mongoose /
+          Sequelize{" "}
         </h6>
       </div>
-      <div className="para">
+      <div className="para-skills ">
         <h3 className="heading">Application</h3>
-        <h6 className="text">
-          Git, Heroku, Insomnia, MySQL, Progressive Web Application, Webpack
+        <h6 className="text-skills">
+          Git / Heroku / Insomnia / MySQL / Progressive Web Application /
+          Webpack
         </h6>
       </div>
-      <div className="para">
+      <div className="para-skills ">
         <h3 className="heading">Productivity</h3>
-        <h6 className="text">Slack, Zoom, Microsoft Office Suits</h6>
+        <h6 className="text-skills">Slack / Zoom / Microsoft Office Suits</h6>
       </div>
     </div>
   );
