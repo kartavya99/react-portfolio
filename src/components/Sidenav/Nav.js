8924a89a3ef68b9b8429d5f9c3475847e80c5127
kartavya <kartavyas@gmail.com>
2022-07-15 17:55:47 +1000

    nav styling with addtional icons

--- src/components/Sidenav/Nav.js
+++ src/components/Sidenav/Nav.js
@@ -1,27 +1,33 @@
 import React from "react";
-
 import pic from "../../Images/pic.jpg";
 import "./Nav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const Nav = ({ currentPage, pageChangeHandler }) => {
   // console.log(pageChangeHandler);
   return (
     <div className="sidenav">
       <img src={pic} alt="tech" className="nav-img"></img>
-      <h1 className="name">Kartavya Saini</h1>
-      <a href="#about" onClick={() => pageChangeHandler("About")} className="">
+      <h1 className="name">KARTAVYA SAINI</h1>
+      <a
+        href="#about"
+        onClick={() => pageChangeHandler("About")}
+        className="a-link"
+      >
         About
       </a>
       <a
         href="#project"
-        className=""
+        className="a-link"
         onClick={() => pageChangeHandler("Project")}
       >
         Project
       </a>
       <a
         href="#skills"
-        className=""
+        className="a-link"
         onClick={() => pageChangeHandler("Skills")}
       >
         {" "}
@@ -29,16 +35,60 @@ const Nav = ({ currentPage, pageChangeHandler }) => {
       </a>
       <a
         href="#contact"
-        className=""
+        className="a-link"
         onClick={() => pageChangeHandler("Contact")}
       >
         {" "}
         Contact{" "}
       </a>
-      <a href="#resume" className="">
+      <a href="#resume" className="a-link">
         {" "}
         Resume{" "}
       </a>
+
+      <footer>
+        <div>
+          <a
+            href="https://github.com/kartavya99"
+            target="__blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="2x"
+              className="p-2 mx-2 text-light"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kartavya99"
+            target="__blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="2x"
+              className="p-2 mx-2 text-light"
+            />
+          </a>
+          <a
+            href="mailto:kartavyas@gmail.com"
+            target="__blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              size="2x"
+              className="p-2 mx-2 text-light"
+            />
+          </a>
+        </div>
+      </footer>
+      <div>
+        <p className="footer-line"> have a nice day !!! </p>
+      </div>
     </div>
   );
 };
