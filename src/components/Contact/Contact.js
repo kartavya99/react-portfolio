5ac406fed9ec9a902b229cb6f9f124bf66ee4e79
kartavya <kartavyas@gmail.com>
2022-07-16 12:10:36 +1000

    added more styles to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const Contact = () => {
   return (
@@ -7,14 +10,58 @@ const Contact = () => {
       <heading>
         <div className="contact-heading"> Contact Me |</div>
       </heading>
-      <p className="text">
-        {" "}
-        Thank you for visiting my portfolio please click below to get in touch{" "}
-      </p>
-      <div className="text"> GitHuub</div>
-      <div className="text"> linkedin</div>
-      <div className="text"> Phone</div>
-      <div className="text"> Email</div>
+
+      <div className="para-contact">
+        <p className="contact-text">
+          {" "}
+          Thank you for visiting my portfolio please click below to get in touch{" "}
+        </p>
+        <div className="contact-text"> GitHuub</div>
+        <div className="contact-text"> linkedin</div>
+        <div className="contact-text"> Phone</div>
+        <div className="contact-text"> Email</div>
+      </div>
+
+      <footer>
+        <div className="div-footer">
+          <a
+            href="https://github.com/kartavya99"
+            target="__blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="2x"
+              className="p-2 mx-2 text-light"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kartavya99"
+            target="__blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="2x"
+              className="p-2 mx-2 text-light"
+            />
+          </a>
+          <a
+            href="mailto:kartavyas@gmail.com"
+            target="__blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              size="2x"
+              className="p-2 mx-2 text-light"
+            />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
