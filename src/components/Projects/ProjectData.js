094f47760c25eb9701b2230a15392fe5e09daed8
kartavya <kartavyas@gmail.com>
2023-04-04 14:32:48 +1000

    new project added

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -1,5 +1,13 @@
 const projectData = [
   {
+    title: "Ecommerce-platform",
+    technology: "MERN & GraphQL",
+    liveUrl: "https://ecomm-platformks.herokuapp.com/",
+    gitHubUrl: "https://github.com/kartavya99/ecommerce-website.git",
+    img: "https://i.imgur.com/VV3ar9Z.png",
+    alt: "A full stack web application build with MERN stack, where business can list their products for marketing and advertising purpose.",
+  },
+  {
     title: "Together Fitness",
     technology: "JavaScript, Handlebars, NodeJs, SQL, Express",
     liveUrl: "https://together-fitness.herokuapp.com/",
@@ -16,13 +24,16 @@ const projectData = [
     alt: "Web application built with REACT framework of JS, where individual can see the historical data of crypto currency based on different duration.",
   },
   {
-    title: "Ecommerce-platform",
-    technology: "MERN & GraphQL",
-    liveUrl: "https://ecomm-platformks.herokuapp.com/",
-    gitHubUrl: "https://github.com/kartavya99/ecommerce-website.git",
-    img: "https://i.imgur.com/VV3ar9Z.png",
-    alt: "A full stack web application build with MERN stack, where business can list their products for marketing and advertising purpose.",
+    title: "Project-Tracker",
+    technology: "HTML5, JavaScript, CSS, JQuery",
+    liveUrl:
+      "https://kartavya99.github.io/Project-Tracker-Bootstrap-JQuery-and-JQueryUI/",
+    gitHubUrl:
+      "https://github.com/kartavya99/Project-Tracker-Bootstrap-JQuery-and-JQueryUI",
+    img: "https://imgur.com/spU9kEj.png",
+    alt: "This is a project tracker application which allows to add the name of project, type of project, the hourly wage for the project, the due date for the project with days until the due date and estimated total earned",
   },
+
   {
     title: "Social Network API",
     technology: "JavaScript, Express, MongoDB",
